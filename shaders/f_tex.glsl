37950d41b3c1a34dbd8ec4f5be53ad1cd951af39
Marcin_Kasznia <kasmar00@gmail.com>
2021-07-04 23:39:26 +0200

    multiple lights better efficiency

--- shaders/f_tex.glsl
+++ shaders/f_tex.glsl
@@ -21,7 +21,7 @@ vec3 calcLight(vec4 normal, vec4 light) {
 
 	float d=0.1;
 	d=abs(distance(fragPos, light));
-	d=d/25;
+	d=d/40;
 	d=1-clamp(d,0,1);
 
 	vec4 ml=normalize(V*light - V*fragPos); //wektor do światła w przestrzeni oka
@@ -40,9 +40,11 @@ void main(void) {
 
 	//Obliczenie modelu oświetlenia
 	//pixelColor= vec4(kd.rgb*d * nl, kd.a);
-	pixelColor=vec4(0,0,0,kd.a);
+	pixelColor=vec4(kd.rgb*0.3,kd.a);
+	vec3 tmp=vec3(0);
 	for (int i=0; i<count; i++){
-		vec3 tmp=calcLight(mn, lights[i]);
-		pixelColor+=vec4(kd.rgb*tmp,0);
+		tmp+=calcLight(mn, lights[i]);
 	}
+	tmp=clamp(tmp,0,1);
+	pixelColor+=vec4(kd.rgb*tmp,0);
 }
